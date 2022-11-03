b8f39b5a5d4c5bbdb4e1a3215e7943f80a5d5044
Lucas Arruda <arrudafdc@gmail.com>
2022-11-03 17:55:13 -0300

    bug da barra lateral resolvido

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 const btn = document.querySelector('.header .btn')
 const menu_hamb = document.querySelector('.menu-hamb')
 const menu = document.querySelector('.menu')
-const sobre = document.querySelector('.sobre')
+const logo = document.querySelector('.logo')
 
 btn.addEventListener('click', () => {
-    sobre.classList.toggle('ativo');
     menu_hamb.classList.toggle('ativo');
     menu.classList.toggle('ativo');
+    logo.classList.toggle('ativo');
 })
-
-
-
