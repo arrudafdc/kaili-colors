78805cb5bd5eab77805c7ce1e18cdf76d6b985f6
Lucas Arruda <arrudafdc@gmail.com>
2022-11-03 22:29:34 -0300

    bug scroll mobile arrumado

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const btn = document.querySelector('.header .btn')
 const menu_hamb = document.querySelector('.menu-hamb')
 const menu = document.querySelector('.menu')
 const logo = document.querySelector('.logo')
+const body = document.querySelector('body')
 
 btn.addEventListener('click', () => {
     menu_hamb.classList.toggle('ativo');
     menu.classList.toggle('ativo');
     logo.classList.toggle('ativo');
+    body.classList.toggle('ativo');
 })
